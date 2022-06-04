107e9da6528c9265f184e5c2067e921193f1a37c
wjsolano <wjsolano@espe.edu.ec>
2022-06-04 18:47:32 -0500

    Agregando segundo template a servicios

--- ProyectoU1/js/servicios.js
+++ ProyectoU1/js/servicios.js
@@ -1,10 +1,11 @@
+//CREA EL ELEMNTO 
 let elem1 = document.createElement('h3');
-
 // Clona el contenido de la plantilla para reutilizarlo múltiples veces
+//templateInicio es el ID
 elem1.append(templateInicio.content.cloneNode(true));
-
-document.body.append(elem1);
 // Ahora el script de <template> se ejecuta
+document.body.append(elem1);
+
 
 elem.onclick = function() {
     elem.attachShadow({mode: 'open'});
@@ -14,7 +15,25 @@ elem.onclick = function() {
     elem.shadowRoot.getElementById('message').innerHTML = "¡Saludos desde las sombras!";
 };
 
+//PARA CUANDO DE CLIC EN EL BOTON ENVIAR
 function enviar(){
     alert("Servicio solicitado correctamente");
 
 }
+
+//PARA EL TEMPLATE DE LA FOTO
+function showContent() {
+    class SellButton extends HTMLElement{
+    constructor(){
+    super();
+    }
+      connectedCallback(){
+      let shadowRoot = this.attachShadow({mode:'open'});
+      const t = document.querySelector('#fotomascota');
+      const instancia = t.content.cloneNode(true);
+      shadowRoot.appendChild(instancia);
+      
+    }
+  }
+  window.customElements.define('aqui-mascota', SellButton);
+  }
