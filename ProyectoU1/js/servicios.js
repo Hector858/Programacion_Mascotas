db66deb8633cf532fec18a4070dc5c9a34461b21
wjsolano <wjsolano@espe.edu.ec>
2022-06-04 14:28:20 -0500

    Primera version templates

--- ProyectoU1/js/servicios.js
+++ ProyectoU1/js/servicios.js
@@ -1,7 +1,20 @@
-let elem = document.createElement('div');
+let elem1 = document.createElement('h3');
 
 // Clona el contenido de la plantilla para reutilizarlo múltiples veces
-elem.append(tmpl.content.cloneNode(true));
+elem1.append(templateInicio.content.cloneNode(true));
 
-document.body.append(elem);
+document.body.append(elem1);
 // Ahora el script de <template> se ejecuta
+
+elem.onclick = function() {
+    elem.attachShadow({mode: 'open'});
+
+    elem.shadowRoot.append(tmpl.content.cloneNode(true)); // (*)
+
+    elem.shadowRoot.getElementById('message').innerHTML = "¡Saludos desde las sombras!";
+};
+
+function enviar(){
+    alert("Servicio solicitado correctamente");
+
+}
