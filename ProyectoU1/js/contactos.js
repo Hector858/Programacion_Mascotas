87322924ab906918be9b03709e3b9cc77666cc11
Hector858 <hicedeno@espe.edu.ec>
2022-06-04 16:23:45 -0500

    aplicacion del primer shadow al boton

--- ProyectoU1/js/contactos.js
+++ ProyectoU1/js/contactos.js
@@ -1,20 +1,45 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<link rel="stylesheet" href="css/contactos.css">
+    <center>
+    <button type="submit" id="greet" onclick=" return validar(); ">ENVIAR</button>
+    </center>
+    
+`;
+
 class Contactos extends HTMLElement{
     constructor(){
         super();
-
+        this.attachShadow({mode:"open"});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
     
 
     connectedCallback(){
-        this.attachShadow({mode:"open"});
+        
         //let elementRoot=document.createElement('div');
 
         this.shadowRoot
       .querySelector("#greet")
       .addEventListener("click", () =>
         window.alert(`¡Hola! Haz sido registrado`)
-      );
+      )
+      ;
+    }
+
+    disconnectedCallback() {
+      this.shadowRoot
+        .querySelector("#greet")
+        .removeEventListener("click", () =>
+          window.alert(`Hey there! I'm ${this.getAttribute("name")}`)
+        );
     }
 }
 
 window.customElements.define('contactos-mascotas', Contactos);
+
+function validar(){
+  formulario.reset();
+  return false;
+}
+
