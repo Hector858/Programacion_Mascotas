d31823a40ee56d8202f036a18a2c12770ce9668e
Hector858 <hicedeno@espe.edu.ec>
2022-06-06 14:21:44 -0500

    Contactos casi terminado

--- ProyectoU1/js/contactos.js
+++ ProyectoU1/js/contactos.js
@@ -1,48 +1,14 @@
-const template = document.createElement("template");
-template.innerHTML = `
-<link rel="stylesheet" href="css/contactos.css">
-<p class="block">
-    <center>
-    <button type="submit" id="greet" onclick=" return validar(); ">ENVIAR</button>
+
+let template = document.createElement("template");
+
+  template.innerHTML = `
+  <link rel="stylesheet" href="css/contactos.css">
+  <p class="block">
+  <center>
+    <a href="contactos.html"><button type="submit" id="greet">ENVIAR</button></a>
     </center>
-    </p>
-`;
-
-/*class Contactos extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({mode:"open"});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-    }
-    
-
-    connectedCallback(){
-        
-        //let elementRoot=document.createElement('div');
-
-        this.shadowRoot
-      .querySelector("#greet")
-      .addEventListener("click", () =>
-        window.alert(`¡Hola! Haz sido registrado ${document.getElementsByName('fullname')[0].value}`)
-      )
-      ;
-    }
-
-    disconnectedCallback() {
-      this.shadowRoot
-        .querySelector("#greet")
-        .removeEventListener("click", () =>
-          window.alert(`Hey there! I'm ${this.getAttribute("name")}`)
-        );
-    }
-}
-
-window.customElements.define('contactos-mascotas', Contactos);*/
-
-function validar(){
-  formulario.reset();
-  return true;
-}
+  </p>
+  `;
 
 class Informacion extends HTMLElement{
   constructor(){
@@ -51,29 +17,32 @@ class Informacion extends HTMLElement{
     let shadowEl=this.attachShadow({mode:'open'});
 
     let elementRoot=document.createElement('div');
-    let elementRoot2=document.createElement('form');
-    elementRoot2.innerHTML= `<link rel="stylesheet" href="css/contactos.css">`;
+    let elementRoot2=document.createElement('form'); 
     let nNamelabel=document.createElement('label');
-    nNamelabel.innerHTML= `<link rel="stylesheet" href="css/contactos.css">Nombre Completo`;
+    nNamelabel.innerHTML= `Nombre Completo`;
     let nNameEl=document.createElement('input');
-    nNameEl.innerHTML= `<link rel="stylesheet" href="css/contactos.css">`;
+    nNameEl.setAttribute('type', 'text');
     let nEmaillabel=document.createElement('label');
-    nEmaillabel.innerHTML= `<link rel="stylesheet" href="css/contactos.css">Email`;
+    nEmaillabel.innerHTML= `Email`;
     let nEmailEl=document.createElement('input');
-    nEmailEl.innerHTML= `<link rel="stylesheet" href="css/contactos.css">`;
+    nEmailEl.setAttribute('type', 'Email');
     let nNumerlabel=document.createElement('label');
-    nNumerlabel.innerHTML= `<link rel="stylesheet" href="css/contactos.css">Número del teléfono`;
+    nNumerlabel.innerHTML= `Número del teléfono`;
     let nNumerEl=document.createElement('input');
+    nNumerEl.setAttribute('type', 'text');
 
     let nDireclabel=document.createElement('label');
-    nDireclabel.innerHTML= `<link rel="stylesheet" href="css/contactos.css">Dirección`;
+    nDireclabel.innerHTML= `Dirección`;
     let nDirecEl=document.createElement('input');
+    nDirecEl.setAttribute('type', 'text');
     let nCedulabel=document.createElement('label');
-    nCedulabel.innerHTML= `<link rel="stylesheet" href="css/contactos.css">Cédula`;
+    nCedulabel.innerHTML= `Cédula`;
     let nCedulEl=document.createElement('input');
+    nCedulEl.setAttribute('type', 'text');
     let nRazalabel=document.createElement('label');
-    nRazalabel.innerHTML= `<link rel="stylesheet" href="css/contactos.css">Raza de perro que desea adoptar`;
+    nRazalabel.innerHTML= `Raza de perro que desea adoptar`;
     let nRazaEl=document.createElement('input');
+    nRazaEl.setAttribute('type', 'text');
 
 
     let parr1=document.createElement('p');
@@ -82,23 +51,23 @@ class Informacion extends HTMLElement{
     let parr4=document.createElement('p');
     let parr5=document.createElement('p');
     let parr6=document.createElement('p');
-    /*elementRoot2.append(document.createElement('p'));*/
+
     parr1.append(nNamelabel);
     parr1.append(nNameEl);
-    /*elementRoot2.append(document.createElement('p'));*/
+    
     parr2.append(nEmaillabel);
     parr2.append(nEmailEl);
-    /*elementRoot2.append(document.createElement('p'));*/
+    
     parr3.append(nNumerlabel);
     parr3.append(nNumerEl);
 
-    /*elementRoot2.append(document.createElement('p'));*/
+    
     parr4.append(nDireclabel);
     parr4.append(nDirecEl);
-    /*elementRoot2.append(document.createElement('p'));*/
+   
     parr5.append(nCedulabel);
     parr5.append(nCedulEl);
-    /*elementRoot2.append(document.createElement('p'));*/
+   
     parr6.append(nRazalabel);
     parr6.append(nRazaEl);
 
@@ -108,6 +77,7 @@ class Informacion extends HTMLElement{
     elementRoot2.append(parr4);
     elementRoot2.append(parr5);
     elementRoot2.append(parr6);
+    elementRoot2.append(template);
 
     let elementRoot1=document.createElement('div');
 
@@ -118,11 +88,14 @@ class Informacion extends HTMLElement{
 
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-      <h4>Mas Info</h4>
-        <ul>
-            <li><i class="fas fa-map-marker-alt"></i> ${nNameEl.value}</li>
-            <li><i class="fas fa-phone"></i> ${nNumerEl.value}</li>
+      <h4>Su Información</h4>
+        <ul> 
+            <li><i class="fa fa-user"></i> ${nNameEl.value}</li>
             <li><i class="fas fa-envelope-open-text"></i> ${nEmailEl.value} </li>
+            <li><i class="fas fa-phone"></i> ${nNumerEl.value}</li>
+            <li><i class="fas fa-map-marker-alt"></i> ${nDirecEl.value}</li>
+            <li><i class="fa fa-id-card"></i> ${nCedulEl.value}</li>
+            <li><i class="fa fa-paw"></i> ${nRazaEl.value} </li>
         </ul>
       
         `;
@@ -131,10 +104,14 @@ class Informacion extends HTMLElement{
     nNameEl.addEventListener('change', this.nameElChage);
     nNumerEl.addEventListener('change', this.nameElChage);
     nEmailEl.addEventListener('change', this.nameElChage);
+    nDirecEl.addEventListener('change', this.nameElChage);
+    nCedulEl.addEventListener('change', this.nameElChage);
+    nRazaEl.addEventListener('change', this.nameElChage);
 
     elementRoot.append(elementRoot2);
     shadowEl.append(elementRoot);
     shadowEl.append(elementRoot1);
+    
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     
   }
@@ -147,18 +124,14 @@ class Informacion extends HTMLElement{
   .querySelector("#greet")
   .addEventListener("click", () =>
     window.alert(`¡Hola! Haz sido registrado`)
-  )
-  .addEventListener("click", () =>
-  formlario.reset()
-  )
-  ;
+  );
 }
 
 disconnectedCallback() {
   this.shadowRoot
     .querySelector("#greet")
     .removeEventListener("click", () =>
-      window.alert(`Hey there! I'm ${this.getAttribute("name")}`)
+      window.alert(`Hey there! I'm User`)
     );
 }
 
