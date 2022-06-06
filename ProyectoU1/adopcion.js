876226365966076c8900158b9526b409ae8d0682
jordan07espi <103512603+jordan07espi@users.noreply.github.com>
2022-06-06 10:53:14 -0500

    Comentario en el archivo JavaScript Adopta

--- ProyectoU1/adopcion.js
+++ ProyectoU1/adopcion.js
@@ -1,6 +1,5 @@
-//Implementación de los ES  MODULS
+//Implementación de los ES6 MODULS
 //Importar el archivo JavaScript de la capeta ped-card
 import PetCard from "./pet-card/adopta.js";
-
 window.customElements.define("pet-card", PetCard);
 
