04c0b25e81f34d429263e7bb47322d24cdb3a98a
Erika <eabenalcazar2@espe.edu.ec>
2022-06-07 00:42:58 -0500

    Elemento personalizado burbujas

--- ProyectoU1/js/index.js
+++ ProyectoU1/js/index.js
@@ -7,13 +7,34 @@ class MyContent extends HTMLElement{
         </style>
         <div class="slider">
             <ul>
-                <li><img src="img/ima1.jpg"></li>
-                <li><img src="img/ima2.jpg"></li>
-                <li><img src="img/ima4.jpg"></li>
-                <li><img src="img/ima3.jpg"></li>
+                <li><img src="img/index/ima1.jpg"></li>
+                <li><img src="img/index/ima2.jpg"></li>
+                <li><img src="img/index/ima4.jpg"></li>
+                <li><img src="img/index/ima3.jpg"></li>
             </ul>
         </div>
         `
     }
 }
 customElements.define('my-content', MyContent);
+
+class Burbujas extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = ` <style src="css/estilos1.css">
+        </style>
+        <div class="burbujas">
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+        </div>
+        `
+    }
+}
+customElements.define('my-bubble', Burbujas);
